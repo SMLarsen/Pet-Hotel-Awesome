c84995d96dccf7b9e7728279c800f3de774142dd
Michael Brannan <michaelbrannan@Michaels-MacBook-Pro.local>
2016-11-18 10:33:45 -0600

    adding owners.js

---
+++ server/routes/owners.js
@@ -0,0 +1,55 @@
+var express = require('express');
+var router = express.Router();
+var pg = require('pg');
+var connectionString = 'postgres://localhost:5432/Sigma';
+
+router.get('/', function(req, res) {
+  console.log('get owners request');
+
+  pg.connect(connectionString, function(err, client, done) {
+    if (err) {
+      console.log('Could not connect to database');
+      res.sendStatus(500);
+    }
+
+    client.query('SELECT * FROM owners', function(err, result) {
+      done();
+
+      if(err) {
+        console.log('select query error: ', err);
+        res.sendStatus(500);
+      }
+      res.send(result.rows);
+
+    });
+
+  });
+
+});
+
+router.post('/', function(req, res) {
+  var newOwner = req.body;
+  pg.connect(connectionString, function(err, client, done) {
+    if(err) {
+      console.log('Could not connect to database');
+      res.sendStatus(500);
+    }
+
+    client.query(
+      'INSERT INTO owners (id, first_name, last_name) ' + 'VALUES ($1, $2, $3)',
+      [newOwner.id, newOwner.first_name, newOwner.last_name],
+      function(err, result) {
+        done();
+
+        if (err) {
+          console.log('insert query error: ', err);
+          res.sendStatus(500);
+        } else{
+          res.sendStatus(201);
+        }
+      }
+    );
+  });
+});
+
+module.exports = router;
